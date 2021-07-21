70edd24576fa03e70bb0cba967b68f1c1d0aa0c6
luis <foreverluiscastro@gmail.com>
2021-07-21 13:08:57 -0400

    filled out migrations for users and comics and completed all routes for the app.

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,15 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  post "/signup", to: "users#create"
+  get "/me", to: "users#show"
+  post "/login", to: "sessions#create"
+  delete "/logout", to: "sessions#destroy"
+  get "/comics", to: "comics#index"
+  post "/comics", to: "comics#create"
+  get "/comics/:id", to: "comics#show"
+  patch "/comics/:id", to: "comics#update"
+  delete "/comics/:id", to: "comics#destroy"
+  get "/allcomics", to: "comics#public_index"
+  get "/allcomics/:id", to: "comics#public_show"
+  get "/marvelcomics", to: "comics#marvel_index"
 end
--- db/migrate/20210721165945_create_comics.rb
+++ db/migrate/20210721165945_create_comics.rb
@@ -1,6 +1,13 @@
 class CreateComics < ActiveRecord::Migration[6.1]
   def change
     create_table :comics do |t|
+      t.string :title
+      t.string :publisher
+      t.string :creators
+      t.string :img_url
+      t.float :price
+      t.string :description
+      t.belongs_to :user, null: false, foreign_key: true
 
       t.timestamps
     end
