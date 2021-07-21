56ffdf10a0eeac5567605fa5814a3917bc42197b
luis <foreverluiscastro@gmail.com>
2021-07-21 12:43:05 -0400

    added create and destroy actions in the sessions controller to handle login and logout requests, respectively.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,23 @@
 class SessionsController < ApplicationController
+    # POST /login
+    def create
+        user = User.find_by(username: params[:username])
+        if user&.authenticate(params[:password])
+            session[:user_id] = user.id
+            render json: user
+        else
+            render json: { errors: ["Invalid username or password"] }, status: :unauthorized
+        end
+    end
+
+    # DELETE /logout
+    def destroy
+    
+        if session[:user_id]
+            session.delete :user_id
+            head :no_content
+        else
+            render json: { errors: []}, status: :unauthorized
+        end
+    end
 end
